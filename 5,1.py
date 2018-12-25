8f28f339e4b12525eb5bed77c31e8e780ff5beda
Shashok666 <44401533+Shashok666@users.noreply.github.com>
2018-12-25 20:43:09 +0300

    Delete 5,1.py

--- 5,1.py
+++
@@ -1,9 +0,0 @@
-k = input("Введите число\n")
-if k.find(".") == 0:
-    print("Число не целое!\n0")
-j = 0
-k = str(k)
-for i in range(len(k)):
-    if  int(k[i]) % 2 == 0:
-        j= j+1
-print("Колличество четных чисел ", j)
